becce8df5e8f8919215efb52efa481d1e6418062
Luke Dashjr <luke-jr+git@utopios.org>
2011-10-04 23:01:26 -0400

    transparent test case (unless it fails)

--- workmaker.py
+++ workmaker.py
@@ -35,11 +35,15 @@ class MerkleTree:
 			f = dblsha(f + s)
 		return f
 
+# MerkleTree test case
 mt = MerkleTree([a2b_hex(a) for a in [
 	'999d2c8bb6bda0bf784d9ebeb631d711dbbbfe1bc006ea13d6ad0d6a2649a971',
 	'3f92594d5a3d7b4df29d7dd7c46a0dac39a96e751ba0fc9bab5435ea5e22a19d',
 	'a5633f03855f541d8e60a6340fc491d49709dc821f3acb571956a856637adcb6',
 	'28d97c850eaf917a4c76c02474b05b70a197eaefb468d21c22ed110afe8ec9e0',
 ]])
-print(b2a_hex(mt.withFirst(a2b_hex('d43b669fb42cfa84695b844c0402d410213faa4f3e66cb7248f688ff19d5e5f7'))))
+assert(
+	b'82293f182d5db07d08acf334a5a907012bbb9990851557ac0ec028116081bd5a' ==
+	b2a_hex(mt.withFirst(a2b_hex('d43b669fb42cfa84695b844c0402d410213faa4f3e66cb7248f688ff19d5e5f7')))
+)
 
