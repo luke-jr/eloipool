c2237698ec9a64f65831b53600a278111a636d95
Luke Dashjr <luke-jr+git@utopios.org>
2011-10-08 00:32:43 -0400

    Workaround: InteractiveConsole direct doesn't like history

--- interactivemode.py
+++ interactivemode.py
@@ -6,11 +6,10 @@ def _RunCLI():
 		namespace = sys.exc_info()[2].tb_frame.f_back.f_back.f_globals
 	
 	namespace['sys'] = sys
-	c = code.InteractiveConsole(locals=namespace)
 	
 	def CLI():
 		while True:
-			c.interact(banner='')
+			code.interact(local=namespace, banner='')
 			print("Not exiting implicitly. Use sys.exit() if you really want to.")
 			dt = ndt = 0
 			for thread in threading.enumerate():
