85387598c03ecea42495d1c618fff4f448065c46
Luke Dashjr <luke-jr+git@utopios.org>
2011-10-07 21:39:50 -0400

    make it difficult to accidentally exit the console

--- interactivemode.py
+++ interactivemode.py
@@ -4,9 +4,21 @@ def _RunCLI():
 		raise None
 	except:
 		namespace = sys.exc_info()[2].tb_frame.f_back.f_back.f_globals
-
+	
+	namespace['sys'] = sys
+	c = code.InteractiveConsole(locals=namespace)
+	
 	def CLI():
-		code.interact(banner=None, local=namespace)
+		while True:
+			c.interact()
+			print("Not exiting implicitly. Use sys.exit() if you really want to.")
+			dt = ndt = 0
+			for thread in threading.enumerate():
+				if thread.daemon:
+					dt += 1
+				else:
+					ndt += 1
+			print("(%d threads: %d primary, %d daemon)" % (dt + ndt, ndt, dt))
 	threading.Timer(0, CLI).start()
 
 _RunCLI()
