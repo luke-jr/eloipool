e99823ab1af447d7fcd6c2f53fbf996324791e54
Luke Dashjr <luke-jr+git@utopios.org>
2012-01-25 21:14:37 -0500

    Bugfix: a2b_hex in Python 3.2 no longer accepts str, only bytes

--- merkletree.py
+++ merkletree.py
@@ -52,12 +52,12 @@ class MerkleTree:
 # MerkleTree test case
 from binascii import a2b_hex, b2a_hex
 mt = MerkleTree([None] + [a2b_hex(a) for a in [
-	'999d2c8bb6bda0bf784d9ebeb631d711dbbbfe1bc006ea13d6ad0d6a2649a971',
-	'3f92594d5a3d7b4df29d7dd7c46a0dac39a96e751ba0fc9bab5435ea5e22a19d',
-	'a5633f03855f541d8e60a6340fc491d49709dc821f3acb571956a856637adcb6',
-	'28d97c850eaf917a4c76c02474b05b70a197eaefb468d21c22ed110afe8ec9e0',
+	b'999d2c8bb6bda0bf784d9ebeb631d711dbbbfe1bc006ea13d6ad0d6a2649a971',
+	b'3f92594d5a3d7b4df29d7dd7c46a0dac39a96e751ba0fc9bab5435ea5e22a19d',
+	b'a5633f03855f541d8e60a6340fc491d49709dc821f3acb571956a856637adcb6',
+	b'28d97c850eaf917a4c76c02474b05b70a197eaefb468d21c22ed110afe8ec9e0',
 ]])
 assert(
 	b'82293f182d5db07d08acf334a5a907012bbb9990851557ac0ec028116081bd5a' ==
-	b2a_hex(mt.withFirst(a2b_hex('d43b669fb42cfa84695b844c0402d410213faa4f3e66cb7248f688ff19d5e5f7')))
+	b2a_hex(mt.withFirst(a2b_hex(b'd43b669fb42cfa84695b844c0402d410213faa4f3e66cb7248f688ff19d5e5f7')))
 )
