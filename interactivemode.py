574ed88c8aaaffd8e323b011e2ae9652bfec96c7
Luke Dashjr <luke-jr+git@utopios.org>
2012-01-27 16:12:47 -0500

    interactivemode: Add exit() function to SIGTERM the entire process

--- interactivemode.py
+++ interactivemode.py
@@ -1,3 +1,7 @@
+def exit():
+	import os, signal
+	os.kill(os.getpid(), signal.SIGTERM)
+
 def _RunCLI():
 	import code, sys, threading
 	try:
@@ -5,12 +9,12 @@ def _RunCLI():
 	except:
 		namespace = sys.exc_info()[2].tb_frame.f_back.f_back.f_globals
 	
-	namespace['sys'] = sys
+	namespace['exit'] = exit
 	
 	def CLI():
 		while True:
 			code.interact(local=namespace, banner='')
-			print("Not exiting implicitly. Use sys.exit() if you really want to.")
+			print("Not exiting implicitly. Use exit() if you really want to.")
 			dt = ndt = 0
 			for thread in threading.enumerate():
 				if thread.daemon:
