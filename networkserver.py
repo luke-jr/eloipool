1decf76ae5fdffbec2bca0361e394884d7747ded
Luke Dashjr <luke-jr+git@utopios.org>
2012-11-06 03:53:57 +0000

    Move handle_readbuf from HTTPHandler to SocketHandler

--- networkserver.py
+++ networkserver.py
@@ -58,6 +58,72 @@ class SocketHandler:
 		
 		self.handle_readbuf()
 	
+	collect_incoming_data = asynchat.async_chat._collect_incoming_data
+	get_terminator = asynchat.async_chat.get_terminator
+	set_terminator = asynchat.async_chat.set_terminator
+	
+	def handle_readbuf(self):
+		while self.ac_in_buffer:
+			lb = len(self.ac_in_buffer)
+			terminator = self.get_terminator()
+			if not terminator:
+				# no terminator, collect it all
+				self.collect_incoming_data (self.ac_in_buffer)
+				self.ac_in_buffer = b''
+			elif isinstance(terminator, int):
+				# numeric terminator
+				n = terminator
+				if lb < n:
+					self.collect_incoming_data (self.ac_in_buffer)
+					self.ac_in_buffer = b''
+					self.terminator = self.terminator - lb
+				else:
+					self.collect_incoming_data (self.ac_in_buffer[:n])
+					self.ac_in_buffer = self.ac_in_buffer[n:]
+					self.terminator = 0
+					self.found_terminator()
+			else:
+				# 3 cases:
+				# 1) end of buffer matches terminator exactly:
+				#    collect data, transition
+				# 2) end of buffer matches some prefix:
+				#    collect data to the prefix
+				# 3) end of buffer does not match any prefix:
+				#    collect data
+				# NOTE: this supports multiple different terminators, but
+				#       NOT ones that are prefixes of others...
+				if isinstance(self.ac_in_buffer, type(terminator)):
+					terminator = (terminator,)
+				termidx = tuple(map(self.ac_in_buffer.find, terminator))
+				try:
+					index = min(x for x in termidx if x >= 0)
+				except ValueError:
+					index = -1
+				if index != -1:
+					# we found the terminator
+					if index > 0:
+						# don't bother reporting the empty string (source of subtle bugs)
+						self.collect_incoming_data (self.ac_in_buffer[:index])
+					specific_terminator = terminator[termidx.index(index)]
+					terminator_len = len(specific_terminator)
+					self.ac_in_buffer = self.ac_in_buffer[index+terminator_len:]
+					# This does the Right Thing if the terminator is changed here.
+					self.found_terminator()
+				else:
+					# check for a prefix of the terminator
+					termidx = tuple(map(lambda a: asynchat.find_prefix_at_end (self.ac_in_buffer, a), terminator))
+					index = max(termidx)
+					if index:
+						if index != lb:
+							# we found a prefix, collect up to the prefix
+							self.collect_incoming_data (self.ac_in_buffer[:-index])
+							self.ac_in_buffer = self.ac_in_buffer[-index:]
+						break
+					else:
+						# no prefix, collect it all
+						self.collect_incoming_data (self.ac_in_buffer)
+						self.ac_in_buffer = b''
+	
 	def push(self, data):
 		if not len(self.wbuf):
 			# Try to send as much as we can immediately
@@ -107,6 +173,7 @@ class SocketHandler:
 	
 	def __init__(self, server, sock, addr):
 		self.ac_in_buffer = b''
+		self.incoming = []
 		self.wbuf = b''
 		self.closeme = False
 		self.server = server
